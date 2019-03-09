72bb968268f4a47a5c6310dff489b7361c48055b
rantarn0326 <rantarn0326@gmail.com>
2019-03-09 12:17:59 +0900

    ABC118C AC

--- AtcoderBeginnerContest/118/c.rb
+++ AtcoderBeginnerContest/118/c.rb
@@ -1,5 +1,7 @@
 N = gets.chomp.to_i
-array = gets.chomp.split(" ").map(&:to_i)
-#arrayの中で、引き算をして最小を作る
-
-#その最小で
+array = gets.split.map(&:to_i)
+g = array.pop
+array.each do |num|
+    g = num.gcd(g)
+end
+puts g
