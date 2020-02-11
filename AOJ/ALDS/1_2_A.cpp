efe72ccf84fe7264f2abefd1718ed3c7b34115d5
yuki-uchida <rantarn0326@gmail.com>
2020-02-11 16:39:48 +0900

    1_6

--- AOJ/ALDS/1_2_A.cpp
+++ AOJ/ALDS/1_2_A.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int bubbleSort(int A[], int N){
+int bubbleSort(int A[], int N)
+{
     int sw = 0;
     bool flag = 1;
-    for (int i = 0; flag; i++){
+    for (int i = 0; flag; i++)
+    {
         flag = 0;
-        for (int j = N - 1; j >= i + 1; j--){
-            if (A[j] < A[j - 1]) {
-                swap(A[j], A[j-1]);
+        for (int j = N - 1; j >= i + 1; j--)
+        {
+            if (A[j] < A[j - 1])
+            {
+                swap(A[j], A[j - 1]);
                 flag = 1;
                 sw++;
             }
@@ -17,13 +21,17 @@ int bubbleSort(int A[], int N){
     return sw;
 }
 
-int main() {
+int main()
+{
     int A[100], N, sw;
     cin >> N;
-    for (int i = 0; i < N; i++) cin >> A[i];
-    sw = bubbleSort(A,N);
-    for (int i = 0; i < N; i++){
-        if (i) cout << " ";
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    sw = bubbleSort(A, N);
+    for (int i = 0; i < N; i++)
+    {
+        if (i)
+            cout << " ";
         cout << A[i];
     }
     cout << endl;
