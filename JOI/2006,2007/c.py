7062878f4657beaac1c5cba18c7c66757ab5c689
yuki-uchida <rantarn0326@gmail.com>
2020-02-26 21:34:23 +0900

    JOI2006,2007 C AC

--- JOI/2006,2007/c.py
+++ JOI/2006,2007/c.py
@@ -1,14 +1,26 @@
 import math
+# n <= 3000
+# 3重for文だと10^9? ダメだろうな・・・
+# でも、二点固定しただけだと対角線なのかわからない
 N = int(input())
 pillars = []
 for i in range(N):
-    pillars.append(list(map(int, input().split(" "))))
-print(pillars)
+    pillars.append(tuple(map(int, input().split())))
+# print(pillars)
 
-
+pillars_set = set(pillars)
 # 対角線の場合もある・・・・
+
+# 反時計回りか時計回りを区別しないため、for文をi+1しない方が良い？？？
 max_area = 0
-for i in range(len(pillars)):
+for i in range(len(pillars) - 1):
     for j in range(i + 1, len(pillars)):
+        x1, y1 = pillars[i]
+        x2, y2 = pillars[j]
+        x3, y3 = x1 + (y2 - y1), y1 + (x1 - x2)  # なんでここx1-x2じゃないんだ
+        x4, y4 = x2 + (y2 - y1), y2 + (x1 - x2)
+        if ((x3, y3) in pillars_set and (x4, y4) in pillars_set):
+            # print([x1, y1], [x2, y2], [x3, y3], [x4, y4])
+            max_area = max(max_area, (x2 - x1)**2 + (y2 - y1)**2)
 
 print(max_area)
