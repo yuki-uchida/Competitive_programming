dc00a5af43156a6219496732a4242edeeaaa05f7
yuki-uchida <rantarn0326@gmail.com>
2020-07-29 01:28:13 +0900

    計算量の削減の補足

--- AtcoderBeginnerContest/097/c.py
+++ AtcoderBeginnerContest/097/c.py
@@ -5,9 +5,10 @@ K = int(input())
 # abc とabaの場合、abaの方が小さい
 
 substrings = set([])
+# 長さK以下のsubstringのみを列挙すればよい
 for i in range(5):
     for j in range(len(s) - i):
         # print(i, j)
         substrings.add(s[j:j + i + 1])
-# print(substrings)
+print(sorted(substrings))
 print(sorted(substrings)[K - 1])
