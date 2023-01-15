578cea33588565e33a2e7f6c5adee4cdde620226
yuki-uchida <yuki.u.nrnwr@gmail.com>
2023-01-15 15:57:10 +0900

    ABC279 B AC

---
+++ AtcoderBeginnerContest/279/rust/src/b.rs
@@ -0,0 +1,12 @@
+use proconio::{input};
+fn main() {
+    input! {
+        s: String,
+        t: String,
+    }
+    if s.contains(&t) {
+        println!("Yes");
+    }else{
+        println!("No");
+    }
+}
