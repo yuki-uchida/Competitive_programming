ac2a46fb454da7dc002bf5db74a287983e5008c6
yuki-uchida <yuki.u.nrnwr@gmail.com>
2023-02-05 20:27:15 +0900

    ABC278 A AC

--- AtcoderBeginnerContest/abc278/src/bin/a.rs
+++ AtcoderBeginnerContest/abc278/src/bin/a.rs
@@ -3,16 +3,14 @@ use std::collections::VecDeque;
 fn main() {
     input! {
         n: usize,
-        k: usize,
+        mut k: usize,
         nums: [usize; n],
     }
-    let mut nums_vec = VecDeque::new();
-    for num in nums {
-        nums_vec.push_back(num);
+    k = k.min(n);
+    for i in k..n {
+        print!("{} ", nums[i]);
     }
-    for _ in 0..k {
-        nums_vec.pop_front();
-        nums_vec.push_back(0);
+    for _ in (n-k)..n {
+        print!("0 ",);
     }
-    println!("{:?}", nums_vec.join(" å"));
 }
