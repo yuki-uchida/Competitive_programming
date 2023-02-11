228a335dae79798d437f65d7ec694946ef2ee6f3
yuki-uchida <yuki.u.nrnwr@gmail.com>
2023-02-11 17:16:20 +0900

    ABC273 A AC

---
+++ AtcoderBeginnerContest/abc269/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::{input};
+fn main() {
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+    }
+    println!("{}", (a+b)*(c-d));
+    println!("Takahashi");
+}
---
+++ AtcoderBeginnerContest/abc273/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::{input};
+fn main() {
+    input! {
+        n: usize,
+    }
+    let mut ans = 1; 
+    for i in 1..=n {
+        ans *= i
+    }
+    println!("{}", ans);
+}
